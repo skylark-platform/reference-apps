42a63b3850b714abf6d436b11c95c62641c61e98
James Wallis <j@wallis.dev>
2022-04-06 20:00:51 +0100

    feat: Add CDK and deployment GitHub Action

---
+++ packages/infra/bin/infra.ts
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+import "source-map-support/register";
+import * as cdk from "aws-cdk-lib";
+import { SkylarkReferenceAppStack } from "../lib/stack";
+import { Builder } from "@sls-next/lambda-at-edge";
+
+console.log("args", process.argv);
+
+// Run the serverless builder, this could be done elsewhere in your workflow
+
+const app = process.env.APP || "media";
+const gitBranch = process.env.GIT_BRANCH;
+
+const stackName = [app, gitBranch].filter((item) => !!item).join("-");
+const description = `${app} Skylark Reference App deployed via CDK`;
+const appPath = `../../apps/${app}`;
+
+const builder = new Builder(appPath, "./build", {
+  args: ["build"],
+  cwd: appPath,
+});
+builder
+  .build()
+  .then(() => {
+    const app = new cdk.App();
+    new SkylarkReferenceAppStack(app, "skylark-reference-app", {
+      stackName,
+      description,
+      env: {
+        account: process.env.CDK_DEFAULT_ACCOUNT,
+        region: "us-east-1",
+      },
+    });
+  })
+  .catch((e: any) => {
+    console.log(e);
+    process.exit(1);
+  });
---
+++ packages/infra/lib/stack.ts
@@ -0,0 +1,12 @@
+import * as cdk from "aws-cdk-lib";
+import { NextJSLambdaEdge } from "@sls-next/cdk-construct";
+
+export class SkylarkReferenceAppStack extends cdk.Stack {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    new NextJSLambdaEdge(this, "NextJsApp", {
+      serverlessBuildOutDir: "./build",
+    });
+  }
+}
