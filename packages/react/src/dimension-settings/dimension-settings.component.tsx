15f8a8f977b014d32bef759545f4d783955cc107
Jahed Miah <jahed04368@gmail.com>
2022-04-28 11:06:49 +0100

    feat: Dimension Settings animations (#16)
    * dimension settings animations

--- packages/react/src/dimension-settings/dimension-settings.component.tsx
+++ packages/react/src/dimension-settings/dimension-settings.component.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
 import { DimensionContent } from "./dimension-content";
 import { DimensionToggle } from "./dimension-toggle";
 import { DimensionRadioButton } from "./dimension-radio-button";
@@ -8,72 +9,84 @@ interface DimensionSettingsProps {
   show?: boolean;
 }
 
+const variants = {
+  hidden: { opacity: 0, y: "90%" },
+  animate: { opacity: 1, y: 0 },
+};
+
 export const DimensionSettings: React.FC<DimensionSettingsProps> = ({
   show: propShow = false,
 }) => {
   const [show, setShow] = useState(propShow);
-
   return (
     <>
       <div className="fixed bottom-0 right-sm-gutter flex md:right-md-gutter lg:right-lg-gutter xl:right-xl-gutter">
         <DimensionToggle variant="open" onClick={() => setShow(true)} />
       </div>
-      {show && (
-        <div
-          className={` fixed bottom-0 left-0 right-0 z-50 block h-[60vh] bg-white md:h-auto`}
-        >
-          <div className="absolute -top-9 right-sm-gutter flex md:right-md-gutter lg:right-lg-gutter xl:right-xl-gutter">
-            <DimensionToggle variant="close" onClick={() => setShow(false)} />
-          </div>
-          <div className="relative h-full overflow-y-auto py-4 px-sm-gutter md:py-12 md:px-md-gutter  lg:px-lg-gutter xl:px-xl-gutter">
-            <div className="flex items-center justify-between">
-              <SkylarkBranding className="w-12 md:w-48" />
-              <div className="ml-2 flex flex-col items-start justify-end text-sm md:flex-row md:items-center">
-                <p className="text-gray-400">{`Demo v1.0 -`}</p>
-                <a
-                  className="text-skylark-blue md:pl-1"
-                  href="mailto:hello@skylarkplatform.com?subject=Enquiry from demo app"
-                  rel="noreferrer"
-                  target="_blank"
-                >
-                  {`hello@skylarkplatform.com`}
-                </a>
+      <AnimatePresence>
+        {show && (
+          <motion.div
+            animate="animate"
+            className={`fixed bottom-0 left-0 right-0 z-50 block h-[80vh] bg-white font-skylark-branding md:h-auto`}
+            exit="hidden"
+            initial="hidden"
+            key="dimension-settings"
+            transition={{ type: "spring", bounce: 0.2 }}
+            variants={variants}
+          >
+            <div className="absolute -top-9 right-sm-gutter flex md:right-md-gutter lg:right-lg-gutter xl:right-xl-gutter">
+              <DimensionToggle variant="close" onClick={() => setShow(false)} />
+            </div>
+            <div className="relative h-full overflow-y-auto py-4 px-sm-gutter md:py-12 md:px-md-gutter  lg:px-lg-gutter xl:px-xl-gutter">
+              <div className="flex items-center justify-between">
+                <SkylarkBranding className="w-12 md:w-48" />
+                <div className="ml-2 flex flex-col items-start justify-end text-sm md:flex-row md:items-center">
+                  <p className="text-gray-400">{`Demo v1.0 -`}</p>
+                  <a
+                    className="text-skylark-blue md:pl-1"
+                    href="mailto:hello@skylarkplatform.com?subject=Enquiry from demo app"
+                    rel="noreferrer"
+                    target="_blank"
+                  >
+                    {`hello@skylarkplatform.com`}
+                  </a>
+                </div>
+              </div>
+              <div className="grid grid-cols-1 gap-8 pt-7 md:grid-cols-2 md:pt-14 lg:grid-cols-4">
+                <DimensionContent label="Time">
+                  <DimensionRadioButton
+                    options={["Current", "Tomorrow"]}
+                    onChange={(value: string) => console.log(value)}
+                  />
+                </DimensionContent>
+                <DimensionContent label="Location">
+                  <DimensionRadioButton
+                    options={["UK", "South Amercia"]}
+                    onChange={(value: string) => console.log(value)}
+                  />
+                </DimensionContent>
+                <DimensionContent label="Language">
+                  <DimensionRadioButton
+                    options={["English", "Spanish"]}
+                    onChange={(value: string) => console.log(value)}
+                  />
+                </DimensionContent>
+                <DimensionContent label="Customer Type">
+                  <DimensionRadioButton
+                    options={[
+                      "Standard",
+                      "Premium",
+                      "Something else",
+                      "One more thing a bit longer",
+                    ]}
+                    onChange={(value: string) => console.log(value)}
+                  />
+                </DimensionContent>
               </div>
             </div>
-            <div className="grid grid-cols-1 gap-8 pt-7 md:grid-cols-2 md:pt-14 lg:grid-cols-4">
-              <DimensionContent label="Time">
-                <DimensionRadioButton
-                  options={["Current", "Tomorrow"]}
-                  onChange={(value: string) => console.log(value)}
-                />
-              </DimensionContent>
-              <DimensionContent label="Location">
-                <DimensionRadioButton
-                  options={["UK", "South Amercia"]}
-                  onChange={(value: string) => console.log(value)}
-                />
-              </DimensionContent>
-              <DimensionContent label="Language">
-                <DimensionRadioButton
-                  options={["English", "Spanish"]}
-                  onChange={(value: string) => console.log(value)}
-                />
-              </DimensionContent>
-              <DimensionContent label="Customer Type">
-                <DimensionRadioButton
-                  options={[
-                    "Standard",
-                    "Premium",
-                    "Something else",
-                    "One more thing a bit longer",
-                  ]}
-                  onChange={(value: string) => console.log(value)}
-                />
-              </DimensionContent>
-            </div>
-          </div>
-        </div>
-      )}
+          </motion.div>
+        )}
+      </AnimatePresence>
     </>
   );
 };
