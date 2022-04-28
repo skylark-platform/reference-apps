977bfa9436de3f1dc756dafe98098b4a093b9bab
James Wallis <j@wallis.dev>
2022-04-28 11:53:51 +0100

    feat: Add favicons using StreamTV logo (#18)

--- apps/media/pages/_document.tsx
+++ apps/media/pages/_document.tsx
@@ -3,7 +3,26 @@ import { Head, Html, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html>
-      <Head />
+      <Head>
+        <link
+          href="/apple-touch-icon.png"
+          rel="apple-touch-icon"
+          sizes="180x180"
+        />
+        <link
+          href="/favicon-32x32.png"
+          rel="icon"
+          sizes="32x32"
+          type="image/png"
+        />
+        <link
+          href="/favicon-16x16.png"
+          rel="icon"
+          sizes="16x16"
+          type="image/png"
+        />
+        <link href="/site.webmanifest" rel="manifest" />
+      </Head>
       <body className="bg-gray-900 font-body">
         <Main />
         <NextScript />
