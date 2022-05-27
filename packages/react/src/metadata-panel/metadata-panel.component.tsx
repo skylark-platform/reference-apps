d8f61545c8eac459b513ae33d649a9b208ac5bb2
Jahed Miah <jahed04368@gmail.com>
2022-05-27 12:56:22 +0100

    css class change on metadata component

--- packages/react/src/metadata-panel/metadata-panel.component.tsx
+++ packages/react/src/metadata-panel/metadata-panel.component.tsx
@@ -12,13 +12,13 @@ export const MetadataPanel: React.FC<MetadataPanelProps> = ({ content }) => (
   <div className="w-full text-xs text-white md:text-base">
     {content.map(({ body, header, icon }) => (
       <div
-        className="grid w-full grid-cols-8 py-2 md:gap-4 md:py-4 lg:grid-cols-10"
+        className="grid w-full grid-cols-8 py-2 md:gap-16 md:py-4 lg:grid-cols-10"
         key={header}
       >
-        <div className="col-span-1 flex justify-center text-xl md:justify-end md:text-2xl">
+        <div className="col-span-1 justify-center text-xl md:justify-end md:text-2xl">
           {icon}
         </div>
-        <div className="col-span-2 flex w-full justify-start pt-0.5 sm:pt-0">
+        <div className="col-span-2 w-full justify-start pt-0.5 sm:pt-0">
           {header}
         </div>
         <div className="col-span-5 justify-start text-gray-400 lg:col-span-7">
