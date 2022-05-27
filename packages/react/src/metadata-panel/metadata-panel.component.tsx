bb509b6b67f1e195ad7f1984a899b4db00af3deb
Jahed Miah <jahed04368@gmail.com>
2022-05-27 15:24:26 +0100

    added padding on header

--- packages/react/src/metadata-panel/metadata-panel.component.tsx
+++ packages/react/src/metadata-panel/metadata-panel.component.tsx
@@ -12,7 +12,7 @@ export const MetadataPanel: React.FC<MetadataPanelProps> = ({ content }) => (
   <div className="w-full text-xs text-white md:text-base">
     {content.map(({ body, header, icon }) => (
       <div
-        className="grid grid-cols-8 py-2 sm:w-7/12 md:w-full md:py-4 lg:grid-cols-10 lg:gap-16"
+        className="grid w-7/12 grid-cols-8 py-2 md:w-full md:py-4 lg:grid-cols-10 lg:gap-16"
         key={header}
       >
         <div className="col-span-1 justify-center text-xl md:justify-end md:text-2xl">
@@ -21,7 +21,7 @@ export const MetadataPanel: React.FC<MetadataPanelProps> = ({ content }) => (
         <div className="col-span-2 w-full justify-start pt-0.5 sm:pt-0">
           {header}
         </div>
-        <div className="col-span-5 justify-start text-gray-400 lg:col-span-7">
+        <div className="col-span-5 mt-0.5 justify-start text-gray-400 md:mt-0 lg:col-span-7">
           {body && Array.isArray(body) ? body.join(", ") : body}
         </div>
       </div>
