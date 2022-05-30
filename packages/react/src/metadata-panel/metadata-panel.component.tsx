7698399e0d55c2ece4d3c2b1e3115b04f9cc7121
Jahed Miah <jahed04368@gmail.com>
2022-05-30 10:33:20 +0100

    left padding and col span change

--- packages/react/src/metadata-panel/metadata-panel.component.tsx
+++ packages/react/src/metadata-panel/metadata-panel.component.tsx
@@ -15,7 +15,7 @@ export const MetadataPanel: React.FC<MetadataPanelProps> = ({ content }) => (
         className="grid grid-cols-5 py-2 md:grid-cols-10 md:py-4 2xl:grid-cols-8"
         key={header}
       >
-        <div className="col-span-2 flex w-full md:col-span-4 2xl:col-span-2">
+        <div className="col-span-1 flex w-full md:col-span-4 2xl:col-span-2">
           <div className="flex text-xl md:text-2xl">{icon}</div>
           <div className="ml-2 flex justify-start pt-0.5 sm:pt-0">{header}</div>
         </div>
