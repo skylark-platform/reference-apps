7a4423d707c0a9dc0c15ac224fe13780dd0c9486
Jahed Miah <jahed04368@gmail.com>
2022-05-30 11:05:59 +0100

    grid col span change

--- packages/react/src/metadata-panel/metadata-panel.component.tsx
+++ packages/react/src/metadata-panel/metadata-panel.component.tsx
@@ -12,14 +12,14 @@ export const MetadataPanel: React.FC<MetadataPanelProps> = ({ content }) => (
   <div className=" text-xs text-white md:text-base">
     {content.map(({ body, header, icon }) => (
       <div
-        className="grid grid-cols-5 py-2 md:grid-cols-10 md:py-4 2xl:grid-cols-8"
+        className="grid grid-cols-6 py-2 sm:grid-cols-5 md:grid-cols-10 md:py-4 2xl:grid-cols-8"
         key={header}
       >
-        <div className="col-span-1 flex w-full md:col-span-4 2xl:col-span-2">
+        <div className="col-span-2 flex w-full sm:col-span-1 md:col-span-4 2xl:col-span-2">
           <div className="flex text-xl md:text-2xl">{icon}</div>
           <div className="ml-2 flex justify-start pt-0.5 sm:pt-0">{header}</div>
         </div>
-        <div className="col-span-3 mt-0.5 flex justify-start text-gray-400 md:col-span-6 md:mt-0 2xl:col-span-4">
+        <div className="col-span-4 mt-0.5 flex justify-start text-gray-400 sm:col-span-3 md:col-span-6 md:mt-0 2xl:col-span-4">
           {body && Array.isArray(body) ? body.join(", ") : body}
         </div>
       </div>
