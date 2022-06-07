e089ff625b8e6869e5ddf80e96fa01d3a4d2e78e
James Wallis <j@wallis.dev>
2022-06-07 12:09:12 +0100

    refactor: add test env variable

--- apps/media/pages/api/env.tsx
+++ apps/media/pages/api/env.tsx
@@ -1,9 +1,12 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-// eslint-disable-next-line prefer-destructuring
-const jwtest = process.env.COGNITO_EMAIL;
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
   res.status(200).json({
-    jwtest: jwtest || "nope",
+    email: process.env.COGNITO_EMAIL as string,
+    password: process.env.COGNITO_PASSWORD as string,
+    region: process.env.COGNITO_AWS_REGION as string,
+    identityPoolId: process.env.COGNITO_IDENTITY_POOL_ID as string,
+    userPoolId: process.env.COGNITO_USER_POOL_ID as string,
+    userPoolWebClientId: process.env.COGNITO_CLIENT_ID as string,
   })
 }
