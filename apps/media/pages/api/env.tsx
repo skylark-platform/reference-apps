b84324f492ef3fd781c1334c0372f09dfc28c762
James Wallis <j@wallis.dev>
2022-06-07 10:41:53 +0100

    refactor: add test env variable

--- apps/media/pages/api/env.tsx
+++ apps/media/pages/api/env.tsx
@@ -1,8 +1,15 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 // eslint-disable-next-line prefer-destructuring
 const jwtest = process.env.JWTEST;
+// eslint-disable-next-line prefer-destructuring
+const serverRuntimeTest = process.env.serverRuntimeTest;
+// eslint-disable-next-line prefer-destructuring
+const customKey = process.env.customKey;
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
-
-  res.status(200).json(jwtest)
+  res.status(200).json({
+    jwtest: jwtest || "nope",
+    serverRuntimeTest: serverRuntimeTest || "nope",
+    customKey: customKey || "nope"
+  })
 }
