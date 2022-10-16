d763f1c7caef9067eb2fb0d5e62dd66236f59bf6
freixedelo <rui.freixedelo@ostmodern.co.uk>
2022-10-16 23:11:43 +0100

    fix: no collectionPage component

--- apps/saas/pages/collection/[slug].tsx
+++ apps/saas/pages/collection/[slug].tsx
@@ -1,50 +1,11 @@
-import { ReactNode } from "react";
 import type { GetServerSideProps, NextPage } from "next";
 import { useRouter } from "next/router";
 import { NextSeo } from "next-seo";
-import {
-  MoviesPageParsedMovie,
-  CollectionPage,
-} from "@skylark-reference-apps/react";
-import { GraphQLMediaObjectTypes } from "@skylark-reference-apps/lib";
 
 import { useCollection } from "../../hooks/useCollection";
-import { Brand, Set, ImageType, Episode, Movie, Season } from "../../types/gql";
-import { MediaObjectFetcher } from "../../components/mediaObjectFetcher";
-import { getSeoDataForObject, SeoObjectData } from "../../lib/getPageSeoData";
-import {
-  convertGraphQLSetType,
-  convertTypenameToEntertainmentType,
-  getFirstRatingValue,
-  getGraphQLImageSrc,
-  getSynopsisByOrderForGraphQLObject,
-  getTitleByOrderForGraphQLObject,
-} from "../../lib/utils";
 
-const CollectionItemDataFetcher: React.FC<{
-  uid: string;
-  children(data: MoviesPageParsedMovie): ReactNode;
-  type: GraphQLMediaObjectTypes;
-}> = ({ uid, children, type }) => (
-  <MediaObjectFetcher type={type} uid={uid}>
-    {(item: Episode | Movie | Brand | Season | Set) => (
-      <>
-        {children({
-          title: getTitleByOrderForGraphQLObject(item, ["short", "medium"]),
-          image: getGraphQLImageSrc(item?.images, ImageType.Thumbnail),
-          uid: item.uid,
-          href: `/${
-            item.__typename === "Set"
-              ? convertGraphQLSetType(item?.type || "")
-              : convertTypenameToEntertainmentType(item.__typename)
-          }/${item.uid}`,
-          releaseDate: item.release_date || "",
-          duration: "1hr 38m",
-        })}
-      </>
-    )}
-  </MediaObjectFetcher>
-);
+import { getSeoDataForObject, SeoObjectData } from "../../lib/getPageSeoData";
+import { getTitleByOrderForGraphQLObject } from "../../lib/utils";
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const seo = await getSeoDataForObject(
@@ -74,9 +35,6 @@ const Collection: NextPage<{ seo: SeoObjectData }> = ({ seo }) => {
   }
 
   const title = collection ? getTitleByOrderForGraphQLObject(collection) : "";
-  const synopsis = collection
-    ? getSynopsisByOrderForGraphQLObject(collection)
-    : "";
 
   return (
     <>
@@ -85,27 +43,6 @@ const Collection: NextPage<{ seo: SeoObjectData }> = ({ seo }) => {
         openGraph={{ images: seo.images }}
         title={title || seo.title}
       />
-      {collection && (
-        <CollectionPage
-          CollectionItemDataFetcher={CollectionItemDataFetcher}
-          bgImage={getGraphQLImageSrc(collection?.images, ImageType.Main)}
-          content={
-            collection?.content?.objects?.map((item) => ({
-              self: "",
-              slug: "",
-              uid: item?.object?.uid || "",
-              type:
-                (item?.object as Episode | Movie | Brand | Season).__typename ||
-                "",
-            })) || []
-          }
-          loading={!collection}
-          rating={getFirstRatingValue(collection?.ratings)}
-          releaseDate={collection?.release_date || ""}
-          synopsis={synopsis}
-          title={title ?? ""}
-        />
-      )}
     </>
   );
 };
