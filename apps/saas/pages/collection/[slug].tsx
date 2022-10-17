b746cc15ce983e45dfd4c21d7db286d366601b79
freixedelo <rui.freixedelo@ostmodern.co.uk>
2022-10-17 10:48:38 +0100

    fix: collectionPage component

--- apps/saas/pages/collection/[slug].tsx
+++ apps/saas/pages/collection/[slug].tsx
@@ -1,9 +1,9 @@
 import type { GetServerSideProps, NextPage } from "next";
 import { useRouter } from "next/router";
 import { NextSeo } from "next-seo";
+import { CollectionPage } from "@skylark-reference-apps/react";
 
 import { useCollection } from "../../hooks/useCollection";
-
 import { getSeoDataForObject, SeoObjectData } from "../../lib/getPageSeoData";
 import { getTitleByOrderForGraphQLObject } from "../../lib/utils";
 
@@ -23,7 +23,9 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
 const Collection: NextPage<{ seo: SeoObjectData }> = ({ seo }) => {
   const { query } = useRouter();
 
-  const { collection, isError } = useCollection(query?.slug as string);
+  const { collection, isLoading, isError } = useCollection(
+    query?.slug as string
+  );
 
   if (isError && !collection) {
     return (
@@ -43,7 +45,18 @@ const Collection: NextPage<{ seo: SeoObjectData }> = ({ seo }) => {
         openGraph={{ images: seo.images }}
         title={title || seo.title}
       />
-      <div>{JSON.stringify(collection)}</div>
+      <CollectionPage
+        bgImage={
+          "https://dl.airtable.com/.attachments/0ef915b9e5390d59aa31f0543fbc5313/e6092e13/Hero-TarantinoCollection-Mobile.jpg"
+        }
+        collectionItemDataFetcher={""}
+        content={[]}
+        loading={isLoading}
+        rating={""}
+        releaseDate={""}
+        synopsis={""}
+        title={""}
+      />
     </>
   );
 };
