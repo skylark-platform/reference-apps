7d728bdaf118a376ed4bf1317acd09f7959c9366
freixedelo <rui.freixedelo@ostmodern.co.uk>
2022-10-18 16:38:39 +0100

    fix: collectionPage component

--- packages/react/src/pages/collection/collection.page.tsx
+++ packages/react/src/pages/collection/collection.page.tsx
@@ -1,16 +1,10 @@
 import React from "react";
 import {
-  formatReleaseDate,
   formatYear,
   GraphQLMediaObjectTypes,
 } from "@skylark-reference-apps/lib";
-import useTranslation from "next-translate/useTranslation";
-import {
-  SkeletonPage,
-  Hero,
-  Header,
-  StandardThumbnail,
-} from "../../components";
+
+import { SkeletonPage, Hero, StandardThumbnail } from "../../components";
 
 export interface CollectionPageParsedContentItem {
   slug: string;
@@ -52,29 +46,19 @@ interface Props {
 export const CollectionPage: React.FC<Props> = ({
   loading,
   bgImage,
-  title,
-  synopsis,
-  releaseDate,
+
   content,
-  rating,
+  title,
   CollectionItemDataFetcher,
 }) => {
-  const { lang } = useTranslation("common");
+  // eslint-disable-next-line no-console
+  console.log(title);
 
   return (
     <div className="mb-20 mt-48 flex min-h-screen w-full flex-col items-center bg-gray-900 font-body">
       <SkeletonPage show={!!loading}>
         <div className="-mt-48"></div>
-        <Hero bgImage={bgImage}>
-          <Header
-            description={synopsis}
-            numberOfItems={content.length || 0}
-            rating={rating}
-            releaseDate={formatReleaseDate(releaseDate, lang)}
-            title={title}
-            typeOfItems="movie"
-          />
-        </Hero>
+        <Hero bgImage={bgImage}></Hero>
         <div className="grid w-full grid-cols-2 gap-x-4 gap-y-6 px-gutter sm:px-sm-gutter md:grid-cols-3 lg:grid-cols-4 lg:px-lg-gutter xl:px-xl-gutter 2xl:grid-cols-6">
           {content.map(({ self, slug, uid, type }) => (
             <CollectionItemDataFetcher
