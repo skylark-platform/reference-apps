beefd24ba3ca00fe01c26bd28a05f6dce0bfcbe8
James Wallis <j@wallis.dev>
2023-06-20 17:43:39 +0100

    feat: reduce concurrent requests to 50, add url query to disable title animation and show kids dimension, add grid set type

--- apps/streamtv/components/grid.tsx
+++ apps/streamtv/components/grid.tsx
@@ -26,7 +26,7 @@ export const Grid = ({
         )}
       </h2>
     )}
-    <div className="grid grid-cols-2 gap-x-4 gap-y-6 px-gutter py-2 sm:px-sm-gutter md:grid-cols-3 md:py-4 lg:grid-cols-4 lg:px-lg-gutter xl:px-xl-gutter 2xl:grid-cols-6">
+    <div className="grid grid-cols-2 gap-x-4 gap-y-6 px-gutter py-2 sm:px-sm-gutter md:grid-cols-3 md:px-md-gutter md:py-4 lg:grid-cols-4 lg:px-lg-gutter xl:px-xl-gutter 2xl:grid-cols-6">
       {objects?.map((object) =>
         object && hasProperty(object, "__typename") ? (
           <Thumbnail
