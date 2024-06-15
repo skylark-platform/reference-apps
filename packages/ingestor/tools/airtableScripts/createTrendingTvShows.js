be6bd7dd940e970e8a5f71aa2f2ec02ab12c4666
James Wallis <j@wallis.dev>
2024-06-15 19:09:28 +0100

    feat: update scripts

--- packages/ingestor/tools/airtableScripts/createTrendingTvShows.js
+++ packages/ingestor/tools/airtableScripts/createTrendingTvShows.js
@@ -2,7 +2,7 @@ let inputConfig = input.config();
 
 console.log(`The value of tmdb_token is ${inputConfig.tmdb_token}`);
 
-const url = `https://api.themoviedb.org/3/trending/movie/day`;
+const url = `https://api.themoviedb.org/3/trending/tv/week`;
 const response = await fetch(url, {
   headers: {
     authorization: `Bearer ${inputConfig.tmdb_token}`,
@@ -16,16 +16,21 @@ let table = base.getTable("Media Content");
 
 let mediaObjectRecords = (
   await table.selectRecordsAsync({
-    fields: ["tmdb_id", "skylark_object_type", "language"],
+    fields: [
+      "tmdb_id",
+      "skylark_object_type",
+      "language",
+      "autofill_tv_seasons_episodes",
+    ],
   })
 ).records;
 
 let skylarkObjectTypes = await table.getField("skylark_object_type").options;
-const movieChoice = skylarkObjectTypes.choices.find(
-  ({ name }) => name === "Movie",
+const brandChoice = skylarkObjectTypes.choices.find(
+  ({ name }) => name === "Brand",
 );
 
-console.log(movieChoice);
+console.log(brandChoice);
 
 let mediaObjects = mediaObjectRecords
   .map((rec) => ({ ...rec, tmdb_id: rec.getCellValue("tmdb_id") }))
@@ -36,17 +41,24 @@ console.log(existingTmdbIds);
 const englishLanguageRecordId = "recdW4ANwDtnhaQn1";
 const portugueseLanguageRecordId = "recoNzbdGmRCXneKG";
 
-const trending = data.results.splice(0, 12);
-const recordsToCreate = trending.filter(
-  ({ id }) => !existingTmdbIds.includes(`${id}`),
-);
+const trending = data.results
+  .filter(
+    ({ id, original_language }) =>
+      !existingTmdbIds.includes(`${id}`) &&
+      original_language.toLowerCase().startsWith("en"),
+  )
+  .sort((a, b) => (a.first_air_date < b.first_air_date ? 1 : -1))
+  .splice(0, 3);
 
-const createConfig = recordsToCreate.map((movie) => ({
+console.log(trending);
+
+const createConfig = trending.map((brand) => ({
   fields: {
-    internal_title: movie.original_title,
-    tmdb_id: `${movie.id}`,
-    skylark_object_type: movieChoice,
+    internal_title: brand.title || brand.original_title,
+    tmdb_id: `${brand.id}`,
+    skylark_object_type: brandChoice,
     language: [{ id: englishLanguageRecordId }],
+    autofill_tv_seasons_episodes: true,
   },
 }));
 console.log(createConfig);
@@ -67,31 +79,31 @@ let translationRecordIds = await translationsTable.createRecordsAsync(
 );
 console.log("Created " + translationRecordIds.length + " translations!");
 
-// Update Trending Movies Set Content
+// Update New TV Releases Set Content
 let setsTable = base.getTable("sets");
 let sets = (
   await setsTable.selectRecordsAsync({ fields: ["content", "external_id"] })
 ).records;
 
-const trendingMovieSet = sets.find(
-  (rec) => rec.getCellValue("external_id") === "streamtv_trending_movies",
+const newTVReleasesSet = sets.find(
+  (rec) => rec.getCellValue("external_id") === "streamtv_new_tv_releases",
 );
 
-console.log(trendingMovieSet);
-if (trendingMovieSet) {
+console.log(newTVReleasesSet);
+if (newTVReleasesSet) {
   const trendingTmdbIds = trending.map(({ id }) => `${id}`);
 
-  const existingObjects = mediaObjects.filter(({ tmdb_id }) =>
-    trendingTmdbIds.includes(tmdb_id),
-  );
+  // Always make HotD a New TV Release for demos
+  const hotdBrandAirtableID = "recexHwDtkWbI2LM8";
   const content = [
-    ...recordIds.map((id) => ({ id })),
-    ...existingObjects.map(({ id }) => ({ id })),
+    { id: recordIds[0] },
+    { id: hotdBrandAirtableID },
+    ...recordIds.splice(1).map((id) => ({ id })),
   ];
+
   console.log(trendingTmdbIds, content);
-  await setsTable.updateRecordAsync(trendingMovieSet, {
+
+  await setsTable.updateRecordAsync(newTVReleasesSet, {
     content: content,
   });
 }
-
-output.set("tmdb_token", inputConfig.tmdb_token);
