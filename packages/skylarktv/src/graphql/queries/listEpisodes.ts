579e3fb4ff86e3e38fe0032c5cb42df47f4e8aff
James Wallis <j@wallis.dev>
2024-10-15 16:16:01 +0100

    feat: add query parameters to episodes and movies pages

--- packages/skylarktv/src/graphql/queries/listEpisodes.ts
+++ packages/skylarktv/src/graphql/queries/listEpisodes.ts
@@ -1,29 +1,55 @@
 import { gql } from "graphql-request";
 
+const episodeListingFragment = gql`
+  fragment episodeListingFragment on EpisodeListing {
+    next_token
+    objects {
+      __typename
+      uid
+      slug
+      title
+      title_short
+      synopsis
+      synopsis_short
+      episode_number
+      release_date
+      images {
+        objects {
+          uid
+          title
+          type
+          url
+        }
+      }
+      tags {
+        objects {
+          uid
+          name
+          type
+        }
+      }
+    }
+  }
+`;
+
 export const LIST_EPISODES = gql`
+  ${episodeListingFragment}
+
   query LIST_EPISODES($nextToken: String) {
     listObjects: listEpisode(next_token: $nextToken, limit: 20) {
-      next_token
-      objects {
-        __typename
-        uid
-        slug
-      }
+      ...episodeListingFragment
     }
   }
 `;
 
 // No Portuguese Genres have been added to the ingestor yet, so only set language on the movies relationship
 export const LIST_EPISODES_BY_GENRE = gql`
+  ${episodeListingFragment}
+
   query LIST_EPISODES_BY_GENRE($uid: String!, $nextToken: String) {
     getObject: getGenre(uid: $uid) {
       episodes(next_token: $nextToken) {
-        next_token
-        objects {
-          __typename
-          uid
-          slug
-        }
+        ...episodeListingFragment
       }
     }
   }
@@ -31,15 +57,12 @@ export const LIST_EPISODES_BY_GENRE = gql`
 
 // No Portuguese Genres have been added to the ingestor yet, so only set language on the movies relationship
 export const LIST_EPISODES_BY_TAG = gql`
+  ${episodeListingFragment}
+
   query LIST_EPISODES_BY_TAG($uid: String!, $nextToken: String) {
     getObject: getSkylarkTag(uid: $uid) {
       episodes(next_token: $nextToken) {
-        next_token
-        objects {
-          __typename
-          uid
-          slug
-        }
+        ...episodeListingFragment
       }
     }
   }
