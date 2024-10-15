4bf684fdc625ea5a51fa3c51fe6ee1e2d2607ded
James Wallis <j@wallis.dev>
2024-10-15 16:45:50 +0100

    feat: update navigaiton

--- packages/skylarktv/src/constants/navigation.tsx
+++ packages/skylarktv/src/constants/navigation.tsx
@@ -7,7 +7,31 @@ export const CLIENT_NAVIGATION_CONFIG: {
 } = {
   links: [
     { localeKey: "home", href: "/", icon: <MdHome /> },
-    { localeKey: "movies", href: "/movies", icon: <MdMovie /> },
+    { localeKey: "shows", href: "/episodes", icon: <MdMovie /> },
+    {
+      localeKey: "music-videos",
+      href: {
+        pathname: "/episodes",
+        query: { tag: "01J9PAMFSGSGKEX6HKK0QDN2Z2" },
+      },
+      icon: <MdMovie />,
+    },
+    {
+      localeKey: "interviews",
+      href: {
+        pathname: "/episodes",
+        query: { tag: "01J9PAMFS321GKYMA67QP44J7E" },
+      },
+      icon: <MdMovie />,
+    },
+    {
+      localeKey: "podcasts",
+      href: {
+        pathname: "/episodes",
+        query: { tag: "01J9PAMFSPSM1MVPW1M2P45TVC" },
+      },
+      icon: <MdMovie />,
+    },
     {
       localeKey: "blog",
       href: "/articles",
